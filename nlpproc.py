3ea6f8b7940122ee39740677a38a1407d4d4e10d
Chinmayee <55998927+chinmayeeet@users.noreply.github.com>
2021-11-17 01:27:18 +0530

    Update nlpproc.py

--- nlpproc.py
+++ nlpproc.py
@@ -5,3 +5,14 @@ pd.set_option('display.max_colwidth',-1)
 data= data[['Sentiment','Tweet']]
 data.head()
 data['Sentiment'].value_counts()
+#removing punctuation from tweet data
+import string
+string.punctuation
+def rem_punct(text):
+    cleanedtext="".join([i for i in text if i not in string.punctuation])
+    return cleanedtext
+data['clean_data']=data['Tweet'].apply(lambda x:rem_punct(x))
+data.head()
+#consistency in data by lowering case
+data['lower_case']=data['clean_data'].apply(lambda x: x.lower())
+data.head()
